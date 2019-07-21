b602b7804d6ed638802f59797241382b01147815
Olof Engström <oeng@kth.se>
2019-07-22 00:18:32 +0200

    Add reference

--- AoC_day03/AoC_day03/Program.cs
+++ AoC_day03/AoC_day03/Program.cs
@@ -36,6 +36,7 @@ namespace AoC_day03
             }
 
             // override object.GetHashCode
+            // https://stackoverflow.com/questions/5221396/what-is-an-appropriate-gethashcode-algorithm-for-a-2d-point-struct-avoiding
             public override int GetHashCode()
             {
                 unchecked
@@ -108,58 +109,3 @@ namespace AoC_day03
         }
     }
 }
-
-
-            // #1 @ 916,616: 21x29
-
-            /*
-            for (int i = 1; i < width + 1; i++)      // For every dot in width  
-            {                                                                   
-                for (int j = 1; j < height + 1; j++) // For every dot in height. Count from the top left corner down to height                           
-                {
-                    // Beacuse the distances we get from left to left and top to top is the dots inbetween, the box starts at the next dot
-                    // Thats why I set i and j to 1.
-                    int partA = fTop + j;   
-                    int partB = fLeft + i;
-                    string partAString = partA.ToString();
-                    string partBString = partB.ToString();
-                    string usedPointString = partBString + partAString; // left + top
-
-                    // I convert the ints to strings just to put them togheter, if I were to add them togheter as ints I would get a number that might..
-                    // - appear again out of coincidence but isn't actually at the same dot. But if I put them togheter as a string, ex, "917" + "617", I get the unique value 917617 that could represent a dot.
-                    // Then I Parse that string back to an int and add it to usedSpace 
-
-                    int usedPoint = int.Parse(usedPointString);
-                    usedSpace.Add(usedPoint);
-                }
-            }
-        }
-
-
-
-        int finalPart1 = 0;     // Count all the duplicates.
-
-        var q = from x in usedSpace
-                group x by x into g
-                let count = g.Count()
-                orderby count descending
-                select new { Value = g.Key, Count = count };
-        foreach (var x in q)
-        {
-            if (x.Count >= 2) // If there are duplicates or triplets or more of a number in usedSpace
-            {
-                finalPart1++;
-            }
-        }
-
-
-        // With the test line { "#1 @ 1,1: 2x2" };
-        // I get these four dots 22, 23, 32, 33, which are correct.
-        // So there might be something wrong with the counter above.
-        // usedSpace.ForEach(i => Console.Write("{0}\n", i));
-
-        Console.WriteLine(finalPart1);
-        Console.WriteLine("Done");
-
-        Console.ReadLine();
-*/
